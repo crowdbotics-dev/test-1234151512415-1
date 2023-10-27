ce3b6e27c84d5315880741e108b6b7a58cb2e20c
Crowdbotics <engineering@crowdbotics.com>
2023-10-27 17:56:51 +0000

    Update store.

--- store/testAPI/api.js
+++ store/testAPI/api.js
@@ -3,38 +3,11 @@ const testAPI = axios.create({
   baseURL: "https://test-1234151512415-1.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-function api_docs_schema_retrieve(payload) {
-  return testAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
-}
 function api_v1_login_create(payload) {
   return testAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
   return testAPI.post(`/api/v1/signup/`, payload)
-}
-function rest_auth_login_create(payload) {
-  return testAPI.post(`/rest-auth/login/`, payload)
-}
-function rest_auth_logout_create(payload) {
-  return testAPI.post(`/rest-auth/logout/`)
-}
-function rest_auth_password_change_create(payload) {
-  return testAPI.post(`/rest-auth/password/change/`, payload)
-}
-function rest_auth_password_reset_create(payload) {
-  return testAPI.post(`/rest-auth/password/reset/`, payload)
-}
-function rest_auth_password_reset_confirm_create(payload) {
-  return testAPI.post(`/rest-auth/password/reset/confirm/`, payload)
-}
-function rest_auth_registration_create(payload) {
-  return testAPI.post(`/rest-auth/registration/`, payload)
-}
-function rest_auth_registration_resend_email_create(payload) {
-  return testAPI.post(`/rest-auth/registration/resend-email/`, payload)
-}
-function rest_auth_registration_verify_email_create(payload) {
-  return testAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
 function rest_auth_user_retrieve(payload) {
   return testAPI.get(`/rest-auth/user/`)
@@ -45,19 +18,46 @@ function rest_auth_user_update(payload) {
 function rest_auth_user_partial_update(payload) {
   return testAPI.patch(`/rest-auth/user/`, payload)
 }
+function api_docs_schema_retrieve(payload) {
+  return testAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+}
+function rest_auth_login_create(payload) {
+  return testAPI.post(`/rest-auth/login/`, payload)
+}
+function rest_auth_logout_create(payload) {
+  return testAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_registration_create(payload) {
+  return testAPI.post(`/rest-auth/registration/`, payload)
+}
+function rest_auth_password_reset_create(payload) {
+  return testAPI.post(`/rest-auth/password/reset/`, payload)
+}
+function rest_auth_password_change_create(payload) {
+  return testAPI.post(`/rest-auth/password/change/`, payload)
+}
+function rest_auth_password_reset_confirm_create(payload) {
+  return testAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+}
+function rest_auth_registration_resend_email_create(payload) {
+  return testAPI.post(`/rest-auth/registration/resend-email/`, payload)
+}
+function rest_auth_registration_verify_email_create(payload) {
+  return testAPI.post(`/rest-auth/registration/verify-email/`, payload)
+}
 export const apiService = {
-  api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  rest_auth_login_create,
-  rest_auth_logout_create,
-  rest_auth_password_change_create,
-  rest_auth_password_reset_create,
-  rest_auth_password_reset_confirm_create,
-  rest_auth_registration_create,
-  rest_auth_registration_resend_email_create,
-  rest_auth_registration_verify_email_create,
   rest_auth_user_retrieve,
   rest_auth_user_update,
-  rest_auth_user_partial_update
+  rest_auth_user_partial_update,
+  api_docs_schema_retrieve,
+  rest_auth_login_create,
+  rest_auth_logout_create,
+  rest_auth_registration_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_resend_email_create,
+  rest_auth_registration_verify_email_create
 }
